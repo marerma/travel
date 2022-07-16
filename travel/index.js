d8f5e8cc42835a7c430d5e0753a8776444eb6807
Maria Ermolaeva <maria.kadantseva@gmail.com>
2022-07-16 22:56:50 +0300

    feat: add popup-function change to login and mobilesize adjustment

--- travel/index.js
+++ travel/index.js
@@ -42,15 +42,6 @@ menuMobile.addEventListener('click', function (event) {
       a.classList.remove('open');
     }
 
-    /*for(let i = 0; i < popUpLinks.length; i++) {
-      const popUpLink = popUpLinks[i]
-      popUpLink.addEventListener('click', function() {
-        openPopup(popUp)
-        openPopup(overlay)
-      }
-      )
-    } */
-
     popUpLinks.forEach((el) => {
       el.addEventListener('click', function() {
         openPopup(popUp)
@@ -63,24 +54,70 @@ menuMobile.addEventListener('click', function (event) {
       closePopUp(overlay)
     })
 
-    const signIn = document.querySelector('.sign-button')
-    
-    
+   
+   const signIn = document.querySelector('.sign-button')
    signIn.addEventListener('click', (event) => {
      event.preventDefault()
      let form = document.querySelector('.get-login-data')
      let useremail = form.elements.useremail.value
      let password = form.elements.userpassword.value
-     alert(`Your Emais: ${useremail} \nYour password: ${password}`) 
-    })
+     if (useremail && password) { 
+      alert(`Your Email: ${useremail} \nYour password: ${password}`)
+      } else if (!useremail) {
+        password? alert('Enter your Email!') : alert('Enter your data!')
+      } else {
+        alert('Enter your password!')
+    }
+  })
+  
    
 
-
-
-    
+  const socialMediaBtns = document.querySelector('.popup_socialmedia-buttons')
+  const header = document.querySelector('.popup__header')
+  const linkForgot = document.querySelector('.question-forgot')
+  const registerQ = document.querySelector('.account-question')
+  const registerLink = document.querySelector('.popup-link')
+  const line =  document.querySelector('.popup-line-account')
+  registerLink.setAttribute('state', 'active')
+  const form = document.querySelector('.get-login-data')
   
 
+  function ChangeLoginToSign () {
+  socialMediaBtns.style.display = "none"
+  linkForgot.style.display = "none"
+  header.textContent = "Create account"
+  signIn.textContent = "Sign Up"
+  registerQ.textContent = "Already have an account?"
+  registerLink.textContent = "Log in"
+  line.style.marginTop  = "15px"
+  registerLink.state = 'inactive'
+  form.style.paddingTop = "0px"
+}
+
+function ChangeSignToLogin () {
+  socialMediaBtns.style.display = "block"
+  linkForgot.style.display = "block"
+  header.textContent = "Log in to your account"
+  signIn.textContent = "Sign In"
+  registerQ.textContent = "Don't have an account?"
+  registerLink.textContent = "Register"
+  registerLink.state = 'active'
+  line.style.marginTop  = "0px"
+  form.style.paddingTop = "10px"
+}
+
+const registration = document.querySelector('.popup-link')
+
+registration.addEventListener('click', () => {
+  if (registerLink.state === 'inactive') {
+      ChangeSignToLogin()
+  } else { ChangeLoginToSign ()
+  }
+})
 
 
+  
+  
+  
 
 /*console.log('Моя самооценка - 75 баллов: \nВёрстка соответствует макету. Ширина экрана 390px +48; \n Ни на одном из разрешений до 320px включительно не появляется горизонтальная полоса прокрутки. Весь контент страницы при этом сохраняется: не обрезается и не удаляется +15; \nНа ширине экрана 390рх и меньше реализовано адаптивное меню +22')*/

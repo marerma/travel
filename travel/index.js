611d2cb323135d1546876dbb6d9767832cae9f68
Maria Ermolaeva <maria.kadantseva@gmail.com>
2022-07-19 14:56:42 +0300

    feat: add mobile slider

--- travel/index.js
+++ travel/index.js
@@ -117,40 +117,159 @@ registration.addEventListener('click', () => {
 
 
 /*SLIDER*/
+const screenWidth = screen.width
+const slider = document.querySelector('.destinations__slider')
+const slides = document.querySelectorAll('.slider-image-box')
+const dots = document.querySelectorAll('.dots')
+const dotOne = document.querySelector('.checker-1')
 
-let slider = document.querySelector('.destinations__slider')
-let slides = document.querySelectorAll('.slider-image-box')
-let dots = document.querySelectorAll('.dots')
-let dotOne = document.querySelector('.checker-1')
+const prev = document.querySelector('.prev')
+const next = document.querySelector('.next')
+const arrows = document.querySelector('.btn-slider')
 
-let position = 0
+let position
+let n
+let m
+const positionMoveFull = 860
+const positionMoveMiddle = 300
+const positionMovePad = 100
 
-function toLeft () {
+
+function removeDotsClass() {
   for (dot of dots) {
     dot.classList.remove('active')
   }
-  slider.style.left = position - 860 + 'px'
-  dots[2].classList.add('active')
+}
+
+function toLeft () {
+  removeDotsClass()
+  slider.style.left = position - m + 'px'
+  dots[dots.length-1].classList.add('active')
 }
 
 function toRight () {
-  for (dot of dots) {
-    dot.classList.remove('active')
-  }
-  slider.style.left = position + 860 + 'px'
-  dots[0].classList.add('active')
+  removeDotsClass()
+  slider.style.left = position + m + 'px'
+  dots[n].classList.add('active')
 }
 
 function toCenter () {
-  for (dot of dots) {
-    dot.classList.remove('active')
-  }
+  removeDotsClass()
   slider.style.left = position + 'px'
-  dots[1].classList.add('active')
+  dots[n + 1].classList.add('active')
 }
 
-slides[0].addEventListener('click', ()=> {toRight()})
-slides[1].addEventListener('click', ()=> {toCenter()})
-slides[2].addEventListener('click', ()=> {toLeft()})
+
+if (screenWidth > 1121) {
+  n = 0
+  position = 0
+  m = positionMoveFull
+  
+slides[n].addEventListener('click', ()=> {toRight()})
+slides[n + 1].addEventListener('click', ()=> {toCenter()})
+slides[dots.length-1].addEventListener('click', ()=> {toLeft()})
+dots[n].addEventListener('click', ()=> {toRight()})
+dots[n + 1].addEventListener('click', ()=> {toCenter()})
+dots[dots.length-1].addEventListener('click', ()=> {toLeft()})
+} 
+
+if (screenWidth > 768 && screenWidth <= 1121 ) {
+  n = 0
+  position = 0
+  m = positionMoveMiddle
+  
+slides[n].addEventListener('click', ()=> {toRight()})
+slides[n + 1].addEventListener('click', ()=> {toCenter()})
+slides[dots.length-1].addEventListener('click', ()=> {toLeft()})
+dots[n].addEventListener('click', ()=> {toRight()})
+dots[n + 1].addEventListener('click', ()=> {toCenter()})
+dots[dots.length-1].addEventListener('click', ()=> {toLeft()})
+}
+
+if (screenWidth > 391 && screenWidth <= 768 ) {
+  n = 0
+  position = 0
+  m = 100
+  
+slides[n].addEventListener('click', ()=> {toRight()})
+slides[n + 1].addEventListener('click', ()=> {toCenter()})
+slides[dots.length-1].addEventListener('click', ()=> {toLeft()})
+dots[n].addEventListener('click', ()=> {toRight()})
+dots[n + 1].addEventListener('click', ()=> {toCenter()})
+dots[dots.length-1].addEventListener('click', ()=> {toLeft()})
+}
 
 
+
+if (screenWidth <= 391) {
+
+ position = 0
+ n = 0
+ m = 375
+ activeDot(n) 
+
+  next.addEventListener('click', ()=> {
+    nextSlide()})
+  prev.addEventListener('click', ()=> {
+    rightSlide()})
+  }
+ 
+
+
+function activeDot (a) {
+  for (dot of dots) {
+   dot.classList.remove('active')
+  }
+  dots[a].classList.add('active')
+}
+
+let index = 0
+
+function nextSlide () {
+  arrows.style.opacity = '0'
+  if (index == slides.length - 1) {
+    index = 0
+    position = 0
+    slider.style.left = 0
+    activeDot(index)    
+  } 
+  else {
+    index++
+    activeDot(index)
+    slider.style.left = position - m + 'px'
+    arrows.style.left -= m + 'px'
+    position = position - m 
+  }
+  setTimeout(function() {
+    arrows.style.opacity = '1'
+  }, 1000)
+
+  }
+
+  function rightSlide () {
+    arrows.style.opacity = '0'
+    if (index === 0) {
+      index = slides.length - 1
+      slider.style.left = -750 + 'px'
+      position = - 750 
+      activeDot(index)
+    }
+     else {
+      index--
+      activeDot(index)
+      slider.style.left = position + m + 'px'
+      arrows.style.left -= m + 'px'
+      position = position + m 
+    }
+    setTimeout(function() {
+      arrows.style.opacity = '1'
+    }, 1000)
+  
+  }
+    
+
+    
+ 
+
+  
+  
